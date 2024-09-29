1442f5bee3d862867dfed8a1275fad494bdf99c8
xiaofan <ningfengzeng@gmail.com>
2024-09-29 14:02:32 +0800

    chore: do some wotk

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,23 @@
 <script setup lang="ts">
-
-import {computed, onMounted, ref} from "vue";
+import { computed, onMounted, ref } from "vue";
+import { Branch, Point } from "./type";
 
 const el = ref<HTMLCanvasElement>();
 const ctx = computed(() => el.value!.getContext('2d')!)
 
-const WIDTH = 1200;
+const WIDTH = 800;
 const HEIGHT = 600;
 
 const pendingTasks: Function[] = []
 
-interface Point {
-  x: number
-  y: number
-}
-
-interface Branch {
-  start: Point,
-  length: number,
-  theta: number
-}
-
 function init() {
-  ctx.value.strokeStyle = '#442525'
+  ctx.value.strokeStyle = 'rgba(55, 65, 81, .3)'
   const branch = {
-    start: {x: WIDTH / 2, y: HEIGHT},
+    start: { x: WIDTH / 2, y: HEIGHT },
     length: 40,
     theta: -Math.PI / 2
   }
-
   step(branch)
-
 }
 
 function step(b: Branch, depth = 0) {
@@ -38,20 +25,20 @@ function step(b: Branch, depth = 0) {
   drawBranch(b)
   if (depth < 2 || Math.random() < 0.5) {
     pendingTasks.push(() =>
-        step({
-          start: end,
-          length: b.length + Math.random() * 10 - 5,
-          theta: b.theta - 0.3 * Math.random()
-        }, depth + 1)
+      step({
+        start: end,
+        length: b.length + Math.random() * 10 - 5,
+        theta: b.theta - 0.3 * Math.random()
+      }, depth + 1)
     )
   }
   if (depth < 2 || Math.random() < 0.5) {
     pendingTasks.push(() =>
-        step({
-          start: end,
-          length: b.length + Math.random() * 10 - 5,
-          theta: b.theta + 0.3 * Math.random()
-        }, depth + 1)
+      step({
+        start: end,
+        length: b.length + Math.random() * 10 - 5,
+        theta: b.theta + 0.3 * Math.random()
+      }, depth + 1)
     )
   }
 }
@@ -76,7 +63,7 @@ function startFrame() {
 startFrame()
 
 function getEndPoint(b: Branch) {
-  const {start, length, theta} = b;
+  const { start, length, theta } = b;
   return {
     x: start.x + length * Math.cos(theta),
     y: start.y + length * Math.sin(theta),
@@ -90,21 +77,26 @@ function lineTo(p1: Point, p2: Point) {
   ctx.value.moveTo(p1.x, p1.y)
   // draw a line to(150, 100)
   ctx.value.lineTo(p2.x, p2.y)
+
+  ctx.value.arc(p2.x, p2.y, 1, 0, Math.PI * 2);
+  ctx.value.fillStyle = '#442525'
+  ctx.value.fill()
+
   if (Math.random() < 0.5) {
     const img = new Image()
     img.src = './public/peach-flower.svg'
-    ctx.value.drawImage(img, p2.x - 5, p2.y - 5)
+    ctx.value.drawImage(img, p2.x - img.width / 2, p2.y - img.height / 2)
   }
   if (Math.random() < 0.3) {
     const img = new Image()
     img.src = './public/peach-leave.svg'
-    ctx.value.drawImage(img, p2.x - 5, p2.y - 5)
+    img.className = "test"
+    ctx.value.drawImage(img, p2.x - img.width / 3, p2.y - img.height / 1)
   }
   ctx.value.stroke()
 }
 
 function drawBranch(b: Branch) {
-
   lineTo(b.start, getEndPoint(b))
 }
 
@@ -112,16 +104,8 @@ onMounted(() => {
   init()
 })
 
-
 </script>
 
 <template>
-  <canvas ref="el" :width="WIDTH" :height="HEIGHT"/>
-  <div>
-
-  </div>
+  <canvas ref="el" width="800" height="600" />
 </template>
-
-<style scoped>
-
-</style>
